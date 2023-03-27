b6ca93cc754326bc9f95737c1da56ddbb60db11a
Noah Vawter <diydsp@yahoo.com>
2023-03-27 07:33:26 -0400

    some iteration on rows.

--- floatPrint.c
+++ floatPrint.c
@@ -21,6 +21,7 @@ void printRow( uint8_t row[] )
   int8_t idx;
   for( idx = ARROUT_LEN - 1; idx >= 0; idx-- )
   {
+    if( idx == ( ARROUT_LEN / 2 ) ){ printf("."); }
     printf("%d",row[idx]);
   }
 }
@@ -30,7 +31,7 @@ void addRow( uint8_t exp, uint8_t arrOut[] )
   uint8_t carry;
   uint8_t tmp;
   
-  
+  printf("exp is: %d\n",exp);
   // find location of decimal point, e.g.
   // "16.0000" -> 2
   // ".125" -> 0
@@ -46,15 +47,17 @@ void addRow( uint8_t exp, uint8_t arrOut[] )
     printf("[%c]",*str[idxLoc]);
     idxLoc++;
   }
+  
   printf("\n");
 
   printf("before add  :");
   printRow(arrOut);
   printf("\n");
   
-  printf("will add row: %s\n", *str );
   // perform addition, Least sig dig first for carries
+  printf("will add row: %s\n", *str );
   int8_t idx;
+  int8_t str_idx = len - 1; // string counts backwards
   for( idx = 0 ; idx < len; idx++ )
   {
     tmp = arrOut[ idx ];
@@ -74,27 +77,23 @@ void floatPrint( float val )
 
   fu.f = val;
   printf("--------------\n");
-    
+  printf("will now print: %f\n", val );
+  
   uint8_t sign =   ( fu.u & 0x80000000 ) >> 31;
   uint8_t exp  = ( ( fu.u & 0x7f000000 ) >> 24 ) - 31;
   uint8_t mant =   ( fu.u & 0x00ffffff );
-  printf("exp = %d\n", exp );
+  printf("exp field = %d\n", exp );
 
   uint8_t arrOut[ ARROUT_LEN ]={0};
 
-  uint8_t expIdx;
-  for( expIdx = 0; expIdx < 67; expIdx++ )
+  uint8_t expBit;
+  for( expBit = 0; expBit < 6; expBit++ )
   {
-    if( exp & ( 1 << expIdx ) )
+    if( exp & ( 1 << expBit ) )
     {
-      if( ( exp >= -32 ) && ( exp <= 32 ) )
-      {
-	printf("---\n");
-	addRow( exp, arrOut );
-      }
-	  
+      printf("---\n");
+      addRow( exp, arrOut );
     }
-
   }
-  
+
 }
--- main.c
+++ main.c
@@ -5,7 +5,11 @@
 int main( int argc, char *argv[])
 {
 
-  floatPrint( 1.0f );
+  floatPrint( 0.25f );
+  return 0;
+  
+  floatPrint( 0.5f );
+  
   floatPrint( 2.0f );
   floatPrint( 4.0f );
   floatPrint( 123.456 );
